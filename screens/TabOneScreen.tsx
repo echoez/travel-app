af1137b86466b65a912cdce8d304b397e017ae8a
Michael Cadet <mcadet@uncommongoods.com>
2022-01-10 16:12:01 -0500

    image asset working

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -2,13 +2,20 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { Image } from 'react-native';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One #####</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.title}>Tab One!</Text>
+      <Image 
+        style={{width: 50, height: 50}}
+        source={require('../assets/images/handwave.png')} 
+      /> 
+      <View style={styles.separator} 
+        lightColor="#eee" 
+        darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
