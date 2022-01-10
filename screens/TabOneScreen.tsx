eefb4f3601f94829fd7e08a8cf78f8ae07c6e444
Michael Cadet <mcadet@uncommongoods.com>
2022-01-10 16:48:00 -0500

    added http request

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,11 +1,32 @@
-import { StyleSheet } from 'react-native';
+
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { Image } from 'react-native';
 import { RootTabScreenProps } from '../types';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, StyleSheet} from 'react-native';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+     try {
+      const response = await fetch('https://reactnative.dev/movies.json');
+      const json = await response.json();
+      setData(json.movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One!</Text>
@@ -13,6 +34,18 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
         style={{width: 50, height: 50}}
         source={require('../assets/images/handwave.png')} 
       /> 
+
+<View>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.title}</Text>
+          )}
+        />
+      )}
+    </View>
       <View style={styles.separator} 
         lightColor="#eee" 
         darkColor="rgba(255,255,255,0.1)" />
